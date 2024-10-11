6d7094dde4c1aae590820f935f7f2ad0b5be29b4
johannus22 <jmasilungan1822@gmail.com>
2024-10-11 10:49:27 +0800

    added a cocky animation

---
+++ js/animations.js
@@ -0,0 +1,6 @@
+import autoAnimate from 'https://cdn.jsdelivr.net/npm/@formkit/auto-animate'
+        
+// animations for every input
+autoAnimate(document.getElementById('input-wrapper'), {
+    duration: 200,
+})
